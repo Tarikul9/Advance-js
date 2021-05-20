d57ff5ac48b9c12d2334beaba3533a92d088bb2b
Tarikul Islam <siamsubu@gmail.com>
2021-05-20 17:17:50 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,25 @@
+// False:
+// false
+// 0
+// ""
+// undefined
+// null
+// NaN
+// Truthy
+// '0',' '[]
+
+
+
+// const name = 'siam';
+
+let name = null;
+console.log(name);
+if(name){
+    console.log("condition is true")
+}
+else{
+    console.log("condition is false")
+}
+
+const name ="siam";
+console.log(name);
